a19237fe42dbed1b3fa268971c2fb2f17df3e81e
huangyy <huangyongyue@tsingning.com>
2016-12-21 08:52:37 +0800

    增加统计实体

---
+++ qingning-server/qingning-common/src/main/java/qingning/common/entity/statistics/ActiveInfo.java
@@ -0,0 +1,25 @@
+package qingning.common.entity.statistics;
+
+public class ActiveInfo {
+	private String device_id;
+	private String ip;
+	private String platform;     //webchat android ios
+	private String device_model;
+	private String device_oem;
+	private String screen_size;
+	private String os_version;
+	private String longitude;
+	private String latitude;
+	private String city;
+	private String city_id;
+	private String province;
+	private String province_id;
+	private String country;
+	private String country_id;
+	private String channel_id;
+	private String version;
+	private String net_status;   //wifi, 4G, 3G
+	private String webchat_authorization;
+	private String live_room_build;
+	private String webchat_notice;
+}
