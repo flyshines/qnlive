75e9a87faf6d0af887d3ccf3199d18012d7d3d15
xueb <1556390393@qq.com>
2016-12-29 09:38:02 +0800

    增加课程share_url

--- qingning-server/qingning-lecturer-server/src/main/java/qingning/lecture/server/imp/LectureServerImpl.java
+++ qingning-server/qingning-lecturer-server/src/main/java/qingning/lecture/server/imp/LectureServerImpl.java
@@ -960,7 +960,7 @@ public class LectureServerImpl extends AbstractQNLiveServer {
         resultMap.put("status",courseMap.get("status"));
         resultMap.put("course_type",courseMap.get("course_type"));
         resultMap.put("course_password",courseMap.get("course_password"));
-        resultMap.put("share_url","");//TODO
+        resultMap.put("share_url","http://test.qnlive.1758app.com/web/#/nav/living/detail?course_id"+reqMap.get("course_id").toString());//TODO
         resultMap.put("course_update_time",courseMap.get("update_time"));
         resultMap.put("course_title",courseMap.get("course_title"));
 
