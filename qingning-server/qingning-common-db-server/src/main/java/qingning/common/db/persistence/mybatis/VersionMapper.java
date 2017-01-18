1e83846befd57e639788780a6937d0e0b072df73
xueb <1556390393@qq.com>
2017-01-18 17:08:38 +0800

    新增版本号数据库相关支持文件

---
+++ qingning-server/qingning-common-db-server/src/main/java/qingning/common/db/persistence/mybatis/VersionMapper.java
@@ -0,0 +1,21 @@
+package qingning.common.db.persistence.mybatis;
+
+import qingning.common.db.persistence.mybatis.entity.Version;
+
+import java.util.Map;
+
+public interface VersionMapper {
+    int deleteByPrimaryKey(Integer versionId);
+
+    int insert(Version record);
+
+    int insertSelective(Version record);
+
+    Version selectByPrimaryKey(Integer versionId);
+
+    int updateByPrimaryKeySelective(Version record);
+
+    int updateByPrimaryKey(Version record);
+
+    Map<String,Object> findVersionInfoByOS(String plateform);
+}
