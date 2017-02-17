a6f82d370a94dac994e5302046ab8f2ac95e7f46
xueb <1556390393@qq.com>
2017-02-17 21:32:19 +0800

    支付的时候插入t_room_distributer_courses表

--- qingning-server/qingning-db-common/src/main/java/qingning/db/common/mybatis/persistence/RoomDistributerCoursesMapper.java
+++ qingning-server/qingning-db-common/src/main/java/qingning/db/common/mybatis/persistence/RoomDistributerCoursesMapper.java
@@ -6,4 +6,8 @@ import java.util.Map;
 public interface RoomDistributerCoursesMapper {
 	List<Map<String, Object>> findRoomDistributerCourseInfo(Map<String, Object> paramters);
 	int insertRoomDistributerCourses(Map<String, Object> paramters);
+
+	Map<String,Object> findRoomDistributerCourse(Map<String, Object> roomDistributerCourseMap);
+
+	int afterStudentBuyCourse(Map<String, Object> roomDistributerCourseInsertMap);
 }
