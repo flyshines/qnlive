893e5538ea768a0e2e9e02685f994a604d27ce2f
gonghs <m18676365713@163.com>
2017-02-18 12:23:22 +0800

    测试

--- qingning-server/qingning-common-server/src/main/java/qingning/common/server/controller/CommonController.java
+++ qingning-server/qingning-common-server/src/main/java/qingning/common/server/controller/CommonController.java
@@ -171,7 +171,8 @@ public class CommonController extends AbstractController {
             logger.info("微信Access_token"+userWeixinAccessToken);
             Map<String, String> param = new HashMap<String, String>();
             param.put("token",userWeixinAccessToken);
-            HttpTookit.doPost("http://test.qnlive.1758app.com/web",param,301);
+            response.sendRedirect("http://test.qnlive.1758app.com/web?token="+userWeixinAccessToken);
+            //HttpTookit.doPost("http://test.qnlive.1758app.com/web",param,301);
         }
     }
 
