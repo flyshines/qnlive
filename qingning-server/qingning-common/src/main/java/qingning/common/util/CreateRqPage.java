9bc0dbed48a7ee0cbb4fd61c14a780da993380d5
gonghs <m18676365713@163.com>
2017-02-24 21:55:47 +0800

    加入 创建二维码图片

---
+++ qingning-server/qingning-common/src/main/java/qingning/common/util/CreateRqPage.java
@@ -0,0 +1,32 @@
+package qingning.common.util;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by DavidGHS on 2017/2/24.
+ * 生成二维码分享链接图片
+ */
+public class CreateRqPage {
+
+    /**
+     * 获取二维码图片
+     * @param icon_img 展示头像图片
+     * @param nick_name 名称
+     * @param qr_url 扫描二维码后的跳转路径
+     */
+    public static BufferedImage getQrCodeImage(String icon_img, String nick_name, String qr_url){
+        return null;
+    }
+
+    /**
+     * 获取二维码图片
+     * @param icon_img 展示头像图片
+     * @param qr_url 名称
+     * @param trunToUrl 扫描二维码后的跳转路径
+     * @param date 时间
+     */
+    public static BufferedImage getQrCodeImage(String icon_img,String qr_url,String trunToUrl,String date){
+        return null;
+    }
+
+}
