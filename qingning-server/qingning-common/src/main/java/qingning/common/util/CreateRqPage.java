912f81a669cb9a5090952093bcb1391ae54dbe3d
fuhao <fuh@tsingning.com>
2017-02-27 11:12:47 +0800

    提交生成图片方法

--- qingning-server/qingning-common/src/main/java/qingning/common/util/CreateRqPage.java
+++ qingning-server/qingning-common/src/main/java/qingning/common/util/CreateRqPage.java
@@ -13,20 +13,22 @@ public class CreateRqPage {
      * @param icon_img 展示头像图片
      * @param nick_name 名称
      * @param qr_url 扫描二维码后的跳转路径
+     * @throws Exception 
      */
-    public static BufferedImage getQrCodeImage(String icon_img, String nick_name, String qr_url){
-        return null;
+    public static BufferedImage getQrCodeImage(String icon_img, String nick_name, String qr_url) throws Exception{
+        return ZXingUtil.createLivePng(icon_img, nick_name, qr_url);
     }
 
     /**
      * 获取二维码图片
      * @param icon_img 展示头像图片
-     * @param qr_url 名称
+     * @param nick_name 名称
      * @param trunToUrl 扫描二维码后的跳转路径
      * @param date 毫秒值 时间戳
+     * @throws Exception 
      */
-    public static BufferedImage getQrCodeImage(String icon_img,String qr_url,String trunToUrl,Long date){
-        return null;
+    public static BufferedImage getQrCodeImage(String icon_img,String nick_name,String trunToUrl,Long date) throws Exception{
+		return	ZXingUtil.createCoursePng(icon_img, nick_name, trunToUrl, date);
     }
 
 }
