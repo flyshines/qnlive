c74247a379efe6c9d8b3972ae74f980ff56d51f2
gonghs <m18676365713@163.com>
2017-03-01 15:30:04 +0800

    图片

--- qingning-server/qingning-common-server/src/main/java/qingning/common/server/imp/CommonServerImpl.java
+++ qingning-server/qingning-common-server/src/main/java/qingning/common/server/imp/CommonServerImpl.java
@@ -2176,7 +2176,7 @@ public class CommonServerImpl extends AbstractQNLiveServer {
         BASE64Encoder encoder = new BASE64Encoder();
         String png_base64 =  encoder.encodeBuffer(bytes);//转换成base64串
         png_base64 = png_base64.replaceAll("\n", "").replaceAll("\r", "");//删除 \r\n
-        png_base64=png_base64.replaceAll("\\+", "!").replaceAll("/", "-");
+        logger.info(png_base64);
         return png_base64;
     }
 
