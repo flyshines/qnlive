a0d6dd8b2fc56364044817a1ea45043aa27514ca
gonghs <m18676365713@163.com>
2017-03-07 10:15:56 +0800

    获取version 信息

--- qingning-server/qingning-common-server/src/main/java/qingning/common/server/imp/CommonServerImpl.java
+++ qingning-server/qingning-common-server/src/main/java/qingning/common/server/imp/CommonServerImpl.java
@@ -181,6 +181,7 @@ public class CommonServerImpl extends AbstractQNLiveServer {
                         versionReturnMap.put("update_desc",versionInfoMap.get("update_desc"));
                         versionReturnMap.put("version_url",versionInfoMap.get("version_url"));
                         versionReturnMap.put("os_audit_version", versionInfoMap.get("os_audit_version"));
+                        resultMap.put("os_audit_version", versionInfoMap.get("os_audit_version"));
                         resultMap.put("version_info", versionReturnMap);
                     }
                 }
