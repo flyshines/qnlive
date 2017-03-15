2edd5f972b0dc65f63b543d3729eab65e4ea5219
liujf <liujf@tsingning.com>
2017-03-15 17:40:32 +0800

    调试微信支付

--- qingning-server/qingning-common-server/src/main/java/qingning/common/server/imp/CommonServerImpl.java
+++ qingning-server/qingning-common-server/src/main/java/qingning/common/server/imp/CommonServerImpl.java
@@ -779,7 +779,22 @@ public class CommonServerImpl extends AbstractQNLiveServer {
             resultMap.put("nonceStr", payResultMap.get("random_char"));
             resultMap.put("signType", "MD5");
             resultMap.put("timeStamp",System.currentTimeMillis()/1000 + "");
-            String paySign  = TenPayUtils.getSign(resultMap, platform);
+
+            String paySign = null;
+            if (isWeb) {
+                paySign = TenPayUtils.getSign(resultMap, platform);
+            } else {
+                SortedMap<String,String> signMap = new TreeMap<>();
+                signMap.put("appid", MiscUtils.getConfigByKey("app_app_id"));
+                signMap.put("partnerid", MiscUtils.getConfigByKey("weixin_app_pay_mch_id"));
+                signMap.put("prepayid", resultMap.get("prepayId"));
+                signMap.put("package", resultMap.get("package"));
+                signMap.put("noncestr", resultMap.get("nonceStr"));
+                signMap.put("signtype", resultMap.get("signType"));
+                signMap.put("timestamp", resultMap.get("timeStamp"));
+                paySign = TenPayUtils.getSign(resultMap, platform);
+            }
+
             resultMap.put ("paySign", paySign);
 
             return resultMap;
