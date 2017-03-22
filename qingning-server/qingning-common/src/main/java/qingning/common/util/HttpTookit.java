6e7beaa6080ce14540814885d41e409947602f3b
gonghs <m18676365713@163.com>
2017-03-22 11:20:00 +0800

    增加效验验证码

--- qingning-server/qingning-common/src/main/java/qingning/common/util/HttpTookit.java
+++ qingning-server/qingning-common/src/main/java/qingning/common/util/HttpTookit.java
@@ -18,6 +18,7 @@ import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -269,6 +270,29 @@ public class HttpTookit {
         }
         return null;
     }
+
+    /**
+     *获取ip地址
+     * @param request 请求
+     * @return
+     */
+    public static String getIpAdress(HttpServletRequest request){
+        String ip = request.getHeader ("X-Real-IP");
+        if (!StringUtils.isBlank (ip) && !"unknown".equalsIgnoreCase (ip)) { return ip; }
+        ip = request.getHeader ("X-Forwarded-For");
+        if (!StringUtils.isBlank (ip) && !"unknown".equalsIgnoreCase (ip)) {
+            // 多次反向代理后会有多个IP值，第一个为真实IP。
+            int index = ip.indexOf (',');
+            if (index != -1) {
+                return ip.substring (0, index);
+            } else {
+                return ip;
+            }
+        } else {
+            return request.getRemoteAddr();
+        }
+    }
+
 
     public static void main(String []args){
 //        Map<String, String> param = new HashMap<String, String>();
