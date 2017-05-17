e3fae7211d01be6bc59f2618101c5b7f32bfa76f
gonghs <gonghs@tsingning.com>
2017-05-17 09:26:21 +0800

    修改绑定服务号相关

--- qingning-server/qingning-lecturer-server/src/main/java/qingning/lecture/server/imp/LectureServerImpl.java
+++ qingning-server/qingning-lecturer-server/src/main/java/qingning/lecture/server/imp/LectureServerImpl.java
@@ -593,28 +593,28 @@ public class LectureServerImpl extends AbstractQNLiveServer {
                 mqRequestEntity.setAppName(appName);
                 this.mqUtils.sendMessage(mqRequestEntity);
             }
-//            if (!MiscUtils.isEmpty(serviceNoMap)) { //该讲师绑定服务号，推送提醒给粉丝 1.判断不为空
-//                log.debug("进入讲师有绑定服务号--------------------:"+serviceNoMap);
-//                String authorizer_access_token = getWeServiceNo(serviceNoMap, userId, serviceNoKey, jedis,appName);
-//                log.debug("验证讲师服务号token--------------------");
-//                if (authorizer_access_token != null) {
-//                    Map<String, Object> wxPushParam = new HashMap<>();
-//                    wxPushParam.put("templateParam", templateMap);//模板消息
-//                    wxPushParam.put("course_id", courseId);//课程ID
-//                    wxPushParam.put("lecturer_id", userId);
-//                    wxPushParam.put("authorizer_appid", serviceNoMap.get("authorizer_appid"));
-//                    wxPushParam.put("accessToken", authorizer_access_token);//课程ID
-//                    wxPushParam.put("pushType", "1");//1创建课程 2更新课程
-//                    log.debug("发送mq消息进行异步处理--------------------");
-//                    RequestEntity mqRequestEntity = new RequestEntity();
-//                    mqRequestEntity.setServerName("MessagePushServer");
-//                    mqRequestEntity.setMethod(Constants.MQ_METHOD_ASYNCHRONIZED);//异步进行处理
-//                    mqRequestEntity.setFunctionName("noticeCourseToServiceNoFollow");
-//                    mqRequestEntity.setParam(getWeCatTemplateInfo(wxPushParam,appName));
-//                    mqRequestEntity.setAppName(appName);
-//                    this.mqUtils.sendMessage(mqRequestEntity);
-//                }
-//            }
+            if (!MiscUtils.isEmpty(serviceNoMap)) { //该讲师绑定服务号，推送提醒给粉丝 1.判断不为空
+                log.debug("进入讲师有绑定服务号--------------------:"+serviceNoMap);
+                String authorizer_access_token = getWeServiceNo(serviceNoMap, userId, serviceNoKey, jedis,appName);
+                log.debug("验证讲师服务号token--------------------");
+                if (authorizer_access_token != null) {
+                    Map<String, Object> wxPushParam = new HashMap<>();
+                    wxPushParam.put("templateParam", templateMap);//模板消息
+                    wxPushParam.put("course_id", courseId);//课程ID
+                    wxPushParam.put("lecturer_id", userId);
+                    wxPushParam.put("authorizer_appid", serviceNoMap.get("authorizer_appid"));
+                    wxPushParam.put("accessToken", authorizer_access_token);//课程ID
+                    wxPushParam.put("pushType", "1");//1创建课程 2更新课程
+                    log.debug("发送mq消息进行异步处理--------------------");
+                    RequestEntity mqRequestEntity = new RequestEntity();
+                    mqRequestEntity.setServerName("MessagePushServer");
+                    mqRequestEntity.setMethod(Constants.MQ_METHOD_ASYNCHRONIZED);//异步进行处理
+                    mqRequestEntity.setFunctionName("noticeCourseToServiceNoFollow");
+                    mqRequestEntity.setParam(getWeCatTemplateInfo(wxPushParam,appName));
+                    mqRequestEntity.setAppName(appName);
+                    this.mqUtils.sendMessage(mqRequestEntity);
+                }
+            }
         }
 
         if ("0".equals(course_type)) {//公开课才开启机器人
