ebc8ea0b4172cf42700f91f6adf4ce69f091c9be
gonghs <gonghs@tsingning.com>
2017-05-23 16:08:41 +0800

    修改微信推送颜色

--- qingning-server/qingning-common/src/main/java/qingning/common/util/Constants.java
+++ qingning-server/qingning-common/src/main/java/qingning/common/util/Constants.java
@@ -12,7 +12,9 @@ public final class Constants {
 	public static final String SYS_FIELD_LAST_UPDATE_TIME ="last_update_time";
 
 
-	public static final String WE_CHAT_PUSH_COLOR = "#FF0000";
+	public static final String WE_CHAT_PUSH_COLOR = "#000000";//黑色
+	public static final String WE_CHAT_PUSH_COLOR_RED = "#FF0000";//红色
+
 
 	public static final String SYS_INSERT_DISTRIBUTER ="INSERT_DISTRIBUTER";
 	public static final String SYS_READ_LAST_COURSE ="SYS_READ_LAST_COURSE";
