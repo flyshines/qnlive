3278a28db503913eeb3f9e64e82122b38da0b4a4
luos <568837624@qq.com>
2017-06-09 15:35:03 +0800

    删除没用包

--- qingning-server/qingning-user-server/src/main/java/qingning/user/server/imp/UserServerImpl.java
+++ qingning-server/qingning-user-server/src/main/java/qingning/user/server/imp/UserServerImpl.java
@@ -3,7 +3,6 @@ package qingning.user.server.imp;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
