e5cbfcd2a911c641dc2f6c87d9e718ac361345bb
gonghs <gonghs@tsingning.com>
2017-06-30 13:53:50 +0800

    user读取series

---
+++ qingning-server/qingning-user-server/src/main/java/qingning/user/server/other/ReadSeriesOperation.java
@@ -0,0 +1,29 @@
+package qingning.user.server.other;
+
+import qingning.common.entity.RequestEntity;
+import qingning.server.rpc.CommonReadOperation;
+import qingning.server.rpc.manager.ILectureModuleServer;
+import qingning.server.rpc.manager.IUserModuleServer;
+
+import java.util.Map;
+
+/**
+ * 包名: qingning.lecture.server.other
+ * 描 述:
+ * 创建日期: 2016/12/4
+ */
+public class ReadSeriesOperation implements CommonReadOperation {
+    private IUserModuleServer userModuleServer;
+
+    public ReadSeriesOperation(IUserModuleServer userModuleServer) {
+        this.userModuleServer = userModuleServer;
+    }
+
+
+    @SuppressWarnings("unchecked")
+	@Override
+    public Object invokeProcess(RequestEntity requestEntity) throws Exception {
+        Map<String, Object> reqMap = (Map<String, Object>) requestEntity.getParam();
+        return userModuleServer.findSeriesBySeriesId(reqMap.get("series_id").toString());
+    }
+}
