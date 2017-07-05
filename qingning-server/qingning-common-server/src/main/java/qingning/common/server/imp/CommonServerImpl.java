c2d2e765d98bfd6b169cb6cd0e9ad6d83c438bec
luos <568837624@qq.com>
2017-07-05 15:05:30 +0800

    支付BUG

--- qingning-server/qingning-common-server/src/main/java/qingning/common/server/imp/CommonServerImpl.java
+++ qingning-server/qingning-common-server/src/main/java/qingning/common/server/imp/CommonServerImpl.java
@@ -1058,8 +1058,8 @@ public class CommonServerImpl extends AbstractQNLiveServer {
         SortedMap<String,String> requestMapData = (SortedMap<String,String>)reqEntity.getParam();
         String outTradeNo = requestMapData.get("out_trade_no");
         String appid = requestMapData.get("appid");
-        //String appName = MiscUtils.getAppNameByAppid(appid);
-        String appName = "qnlive";
+        String appName = MiscUtils.getAppNameByAppid(appid);
+        //String appName = "qnlive";
         Jedis jedis = jedisUtils.getJedis(appName);
         Map<String,Object> billMap = commonModuleServer.findTradebillByOutTradeNo(outTradeNo);
         if(billMap != null && billMap.get("status").equals("2")){
@@ -1067,8 +1067,8 @@ public class CommonServerImpl extends AbstractQNLiveServer {
             return TenPayConstant.SUCCESS;
         }
 
-        //if (TenPayUtils.isValidSign(requestMapData,appName)){// MD5签名成功，处理课程打赏\购买课程等相关业务
-            if(true){
+        if (TenPayUtils.isValidSign(requestMapData,appName)){// MD5签名成功，处理课程打赏\购买课程等相关业务
+            //if(true){
             logger.debug(" ===> 微信notify Md5 验签成功 <=== ");
 
             if("SUCCESS".equals(requestMapData.get("return_code")) &&
@@ -1299,6 +1299,7 @@ public class CommonServerImpl extends AbstractQNLiveServer {
                     jedis.hset(courseKey,  "extra_amount", MiscUtils.convertObjectToLong(sumInfo.get("lecturer_profit"))+"");
                 }else if("2".equals(profit_type)){
                     //系列课收益统计
+                    query.clear();
                     query.put("series_id", courseId);
                     String seriesKey  = MiscUtils.getKeyOfCachedData(Constants.CACHED_KEY_SERIES, query);
                     query.clear();
