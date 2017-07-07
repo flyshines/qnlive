0e98aaca7b1318358f3ab730d48de32acad21e6f
gonghs <gonghs@tsingning.com>
2017-07-07 10:18:20 +0800

    修改发布课程 记性30分钟判断

--- qingning-server/qingning-lecturer-server/src/main/java/qingning/lecture/server/imp/LectureServerImpl.java
+++ qingning-server/qingning-lecturer-server/src/main/java/qingning/lecture/server/imp/LectureServerImpl.java
@@ -373,15 +373,19 @@ public class LectureServerImpl extends AbstractQNLiveServer {
 
         //课程之间需要间隔三十分钟
         String lecturerCoursesAllKey = MiscUtils.getKeyOfCachedData(Constants.CACHED_KEY_COURSE_ALL, query);
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(startTime);
-        cal.add(Calendar.MINUTE, -3*Constants.COURSE_MAX_INTERVAL);
-        long preStartTime = cal.getTimeInMillis();
-        cal.setTimeInMillis(startTime);
-        cal.add(Calendar.MINUTE, 3*Constants.COURSE_MAX_INTERVAL);
-        long nextStartTime = cal.getTimeInMillis();
-        Set<Tuple> courseList = jedis.zrangeByScoreWithScores(lecturerCoursesAllKey, preStartTime+"", nextStartTime+"", 0, 1);
-        if(!MiscUtils.isEmpty(courseList)){
+//        Calendar cal = Calendar.getInstance();//获取日历类
+//        cal.setTimeInMillis(startTime);
+//        cal.add(Calendar.MINUTE, -3*Constants.COURSE_MAX_INTERVAL);
+//        long preStartTime = cal.getTimeInMillis();
+//        cal.setTimeInMillis(startTime);
+//        cal.add(Calendar.MINUTE, 3*Constants.COURSE_MAX_INTERVAL);
+//        long nextStartTime = cal.getTimeInMillis();
+//        Set<Tuple> courseList = jedis.zrangeByScoreWithScores(lecturerCoursesAllKey, preStartTime+"", nextStartTime+"", 0, 1);
+
+        long startIndex = startTime-30*60*1000;
+        long endIndex = startTime+30*60*1000;
+        Long aLong = jedis.zremrangeByScore(lecturerCoursesAllKey, startIndex, endIndex);
+        if(aLong != 0){
             throw new QNLiveException("100029");
         }
         reqMap.put("user_id", userId);
@@ -671,8 +675,7 @@ public class LectureServerImpl extends AbstractQNLiveServer {
         jedis.hmset(messageKey, result);
         //</editor-fold>
         //设置讲师更新课程 30分钟
-        long lpos = MiscUtils.convertInfoToPostion(MiscUtils.convertObjectToLong(course.get("start_time")) , MiscUtils.convertObjectToLong(course.get("position")));
-        jedis.zadd(lecturerCoursesAllKey,lpos,course.get("course_id"));
+        jedis.zadd(lecturerCoursesAllKey,MiscUtils.convertObjectToLong(course.get("start_time")),course.get("course_id"));
         return resultMap;
     }
 
