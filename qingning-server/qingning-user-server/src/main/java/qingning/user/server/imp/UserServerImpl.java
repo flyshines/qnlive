c756ace695220452d58bbe63d8bfcdf6ac55961c
gonghs <gonghs@tsingning.com>
2017-07-11 14:15:15 +0800

    修改 用户查看课程列表

--- qingning-server/qingning-user-server/src/main/java/qingning/user/server/imp/UserServerImpl.java
+++ qingning-server/qingning-user-server/src/main/java/qingning/user/server/imp/UserServerImpl.java
@@ -230,20 +230,36 @@ public class UserServerImpl extends AbstractQNLiveServer {
             }else{ //首页
                 getCourseIdKey = Constants.CACHED_KEY_PLATFORM_COURSE_PREDICTION;
             }
+//            if(MiscUtils.isEmpty(courseId)){//如果没有传入courceid 那么就是最开始的查询  进行倒叙查询 查询现在的
+//                long courseScoreByRedis = MiscUtils.convertInfoToPostion(System.currentTimeMillis(),0L);
+//                startIndex = courseScoreByRedis+"";//设置起始位置
+//                endIndex = "-inf";//设置结束位置
+//            }else{//传了courseid
+//                Map<String,String> queryParam = new HashMap<String,String>();
+//                queryParam.put("course_id", courseId);
+//                RequestEntity requestParam = this.generateRequestEntity(null, null, null, queryParam);
+//                Map<String, String> course = CacheUtils.readCourse(courseId, requestParam, readCourseOperation, jedis, true);//获取当前课程参数
+//                long courseScoreByRedis = MiscUtils.convertInfoToPostion(MiscUtils.convertObjectToLong(course.get("start_time")),  MiscUtils.convertObjectToLong(course.get("position")));//拿到当前课程在redis中的score
+//                startIndex = ""+courseScoreByRedis;//设置起始位置 '(' 是要求大于这个参数
+//                endIndex = "-inf";//设置结束位置
+//            }
             if(MiscUtils.isEmpty(courseId)){//如果没有传入courceid 那么就是最开始的查询  进行倒叙查询 查询现在的
                 long courseScoreByRedis = MiscUtils.convertInfoToPostion(System.currentTimeMillis(),0L);
-                startIndex = courseScoreByRedis+"";//设置起始位置
-                endIndex = "-inf";//设置结束位置
+                startIndex = "-inf";//设置起始位置 '(' 是要求大于这个参数
+                endIndex =courseScoreByRedis+"";//设置结束位置
             }else{//传了courseid
                 Map<String,String> queryParam = new HashMap<String,String>();
                 queryParam.put("course_id", courseId);
                 RequestEntity requestParam = this.generateRequestEntity(null, null, null, queryParam);
                 Map<String, String> course = CacheUtils.readCourse(courseId, requestParam, readCourseOperation, jedis, true);//获取当前课程参数
                 long courseScoreByRedis = MiscUtils.convertInfoToPostion(MiscUtils.convertObjectToLong(course.get("start_time")),  MiscUtils.convertObjectToLong(course.get("position")));//拿到当前课程在redis中的score
-                startIndex = ""+courseScoreByRedis;//设置起始位置 '(' 是要求大于这个参数
-                endIndex = "-inf";//设置结束位置
+                startIndex = "("+courseScoreByRedis;//设置起始位置 '(' 是要求大于这个参数
+                endIndex =MiscUtils.convertInfoToPostion(System.currentTimeMillis(),0L)+"";//设置结束位置
             }
-            courseIdSet = jedis.zrevrangeByScore(getCourseIdKey,startIndex,endIndex,offset,pageCount); //顺序找出couseid  (正在直播或者预告的)
+            courseIdSet = jedis.zrangeByScore(getCourseIdKey,startIndex,endIndex,offset,pageCount); //顺序找出couseid  (正在直播或者预告的)
+
+
+            //courseIdSet = jedis.zrevrangeByScore(getCourseIdKey,startIndex,endIndex,offset,pageCount); //顺序找出couseid  (正在直播或者预告的)
             for(String course_id : courseIdSet){//遍历已经查询到的课程在把课程列表加入到课程idlist中
                 courseIdList.add(course_id);
             }
@@ -286,7 +302,7 @@ public class UserServerImpl extends AbstractQNLiveServer {
                 RequestEntity requestParam = this.generateRequestEntity(null, null, null, queryParam);
                 Map<String, String> course = CacheUtils.readCourse(courseId, requestParam, readCourseOperation, jedis, true);//获取当前课程参数
                 long courseScoreByRedis = MiscUtils.convertInfoToPostion(MiscUtils.convertObjectToLong(course.get("start_time")),  MiscUtils.convertObjectToLong(course.get("position")));//拿到当前课程在redis中的score
-                startIndex = ""+courseScoreByRedis;//设置起始位置 '(' 是要求大于这个参数
+                startIndex = "("+courseScoreByRedis;//设置起始位置 '(' 是要求大于这个参数
                 endIndex ="+inf";//设置结束位置
             }
             courseIdSet = jedis.zrangeByScore(getCourseIdKey,startIndex,endIndex,offset,pageCount); //顺序找出couseid  (正在直播或者预告的)
