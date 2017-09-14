b83a9a4f2100427d02ee3011fe0d81bcf8577c7d
luos <568837624@qq.com>
2017-09-14 13:37:51 +0800

    日志修复

--- qingning-server/qingning-common/src/main/java/qingning/server/advice/exception/WebExceptionAspect.java
+++ qingning-server/qingning-common/src/main/java/qingning/server/advice/exception/WebExceptionAspect.java
@@ -1,10 +1,12 @@
 package qingning.server.advice.exception;
+import com.alibaba.fastjson.JSONObject;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import qingning.server.advice.ExceptionUtils;
@@ -53,7 +55,7 @@ public class WebExceptionAspect {
         HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
         response.reset();
         response.setCharacterEncoding("UTF-8");
-        response.setHeader("Content-Type", "text/plain;charset=UTF-8");
+        response.setHeader("Content-Type", "application/json;charset=UTF-8");
         response.setHeader("icop-content-type", "exception");
         PrintWriter writer = null;
         try {
