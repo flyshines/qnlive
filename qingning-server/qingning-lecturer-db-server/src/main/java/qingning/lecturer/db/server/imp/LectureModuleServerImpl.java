1f6ef6fa4a8c40af6a8ca11f9f03e2ba54b8c97a
gonghs <gonghs@tsingning.com>
2017-09-14 21:34:48 +0800

    修改SQL错误

--- qingning-server/qingning-lecturer-db-server/src/main/java/qingning/lecturer/db/server/imp/LectureModuleServerImpl.java
+++ qingning-server/qingning-lecturer-db-server/src/main/java/qingning/lecturer/db/server/imp/LectureModuleServerImpl.java
@@ -222,7 +222,7 @@ public class LectureModuleServerImpl implements ILectureModuleServer {
 
 	@Override
 	public Map<String, Object> updateCourse(Map<String, Object> reqMap) {
-		Integer updateCount = null;
+		Integer updateCount = 0;
 		Date now = (Date)reqMap.get("now");
 		Map<String,Object> course = new HashMap<String,Object>();
 		course.put("course_id", reqMap.get("course_id"));
