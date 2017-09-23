868e6e57eeadcfe191f4c26af97b5aa519e644c7
gonghs <gonghs@tsingning.com>
2017-09-23 13:47:42 +0800

    1

--- qingning-server/qingning-shop-server/src/main/java/qingning/shop/server/imp/ShopServerImpl.java
+++ qingning-server/qingning-shop-server/src/main/java/qingning/shop/server/imp/ShopServerImpl.java
@@ -2,9 +2,11 @@ package qingning.shop.server.imp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import qingning.common.entity.QNLiveException;
 import qingning.common.entity.RequestEntity;
 import qingning.common.util.AccessTokenUtil;
 import qingning.common.util.Constants;
+import qingning.common.util.MiscUtils;
 import qingning.server.AbstractQNLiveServer;
 import qingning.server.annotation.FunctionName;
 import qingning.server.rpc.initcache.*;
@@ -13,6 +15,7 @@ import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.jar.JarEntry;
 
 public class ShopServerImpl extends AbstractQNLiveServer {
@@ -42,7 +45,32 @@ public class ShopServerImpl extends AbstractQNLiveServer {
         Jedis jedis = jedisUtils.getJedis();
         Map query = new HashMap();
         query.put(Constants.CACHED_KEY_LECTURER_FIELD,lecturer_id);
-        readLecturer(lecturer_id,this.generateRequestEntity(null, null, null, query),readLecturerOperation,jedis);
+        Map<String,String> lecturerMap = readLecturer(lecturer_id,this.generateRequestEntity(null, null, null, query),readLecturerOperation,jedis);
+        //判断当前用户是否是讲师
+        if(MiscUtils.isEmpty(lecturerMap)){
+            throw new QNLiveException("100001");
+        }
+
+
+        if(reqMap.get("good_type").toString().equals("0")){
+            //课程之间需要间隔三十分钟
+            Long startTime = (Long)reqMap.get("start_time");
+            String lecturerCoursesAllKey = MiscUtils.getKeyOfCachedData(Constants.CACHED_KEY_COURSE_ALL, query);
+            long startIndex = startTime-30*60*1000;
+            long endIndex = startTime+30*60*1000;
+            long start = MiscUtils.convertInfoToPostion(startIndex , 0L);
+            long end = MiscUtils.convertInfoToPostion(endIndex , 0L);
+            Set<String> aLong = jedis.zrangeByScore(lecturerCoursesAllKey, start, end);
+            for(String course_id : aLong){
+                Map<String,Object> map = new HashMap<>();
+                map.put("course_id",course_id);
+                Map<String, String> course = readCourse(course_id, generateRequestEntity(null, null, null, map), readCourseOperation, jedis, true);
+                if(course.get("status").equals("1")){
+                    throw new QNLiveException("100029");
+                }
+            }
+        }
+
 
 
 
